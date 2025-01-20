7445de7d01bbe64c1e1645bd29ba02530eaa00ec
RedEyeSH <97534002+RedEyeSH@users.noreply.github.com>
2025-01-21 01:58:15 +0200

    first commit

---
+++ 1.1/src/ServicePoint_05.java
@@ -0,0 +1,27 @@
+// 1.1 - Task 05
+import java.util.Scanner;
+import java.util.LinkedList;
+
+public class ServicePoint_05 {
+    private LinkedList<Customer_03> customerQueue = new LinkedList<>();
+
+    public void addToQueue(Customer_03 a) {
+
+    }
+
+    public Customer_03 removeQueue() {
+        return null;
+    }
+
+    public void serve() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
+class CustomerGenerator {
+
+}
